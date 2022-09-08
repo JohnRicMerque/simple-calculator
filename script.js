60cdfce1f6001dae1aaff1769e1566e6f1bcfb09
JohnRicMerque <johnricmer@gmail.com>
2022-09-08 16:25:38 +0800

    calculator computations are functional

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/* This calculator is guided by WebDev Simplified Tutorial on YouTube
+   The algorithm used is parallel.*/
+
+// Object declaration and method assignment
 class Calculator {
     constructor(previousOperandDisplay, currentOperandDisplay) {
         this.previousOperandDisplay = previousOperandDisplay
@@ -12,7 +16,9 @@ class Calculator {
 
     updateDisplay() {
         this.currentOperandDisplay.innerText = this.currentOperand
-        this.previousOperandDisplay.innerText = this.previousOperand
+            if (this.operation != null){
+               this.previousOperandDisplay.innerText = `${this.previousOperand} ${this.operation}` 
+            }
     }
 
     clear() {
@@ -22,16 +28,44 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     chooseOperation(operation){
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
         this.operation = operation
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
+
     }
 
     compute() {
+        let computation
+        const previous = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(previous) || isNaN(current)) return
+        switch (this.operation){
+            case '+': 
+                computation = previous + current
+                break
+            case '-': 
+                computation = previous - current
+                break
+            case 'x': 
+                computation = previous * current
+                break
+            case '/': 
+                computation = previous / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.opration = undefined
+        this.previousOperand = ''
 
     }
 }
@@ -43,9 +77,9 @@ const previousOperandDisplay = document.querySelector('[data-previous-display]')
 const currentOperandDisplay = document.querySelector('[data-current-display]')
 const allClearBtn = document.querySelector('[data-clear]')
 const deleteBtn = document.querySelector('[data-del]')
-const EqualsBtn = document.querySelector('[data-equals]')
+const equalsBtn = document.querySelector('[data-equals]')
 
-// Oject declaration and method assignment
+// creating instance of object Calculator and assigning methods on corresponding HTML element
 const calculator = new Calculator(previousOperandDisplay, currentOperandDisplay)
 
 numberBtns.forEach(button => {
@@ -60,4 +94,19 @@ operationBtns.forEach(button => {
         calculator.chooseOperation(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+equalsBtn.addEventListener('click', button => {
+        calculator.compute()
+        calculator.updateDisplay()
+    })
+
+allClearBtn.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteBtn.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
